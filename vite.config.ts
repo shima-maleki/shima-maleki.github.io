ce246af8f0eba069040a4fc0fd0c3166cb29aea0
Shima Maleki <92548365+shima-maleki@users.noreply.github.com>
2025-07-24 23:37:41 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/shima-maleki-portfolio/', // Replace with your repository name
+  base: 'shima-maleki.github.io', // Replace with your repository name
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './client/src'),
