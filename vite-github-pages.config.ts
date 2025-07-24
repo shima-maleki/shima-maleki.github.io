e13d2b4790771fdf70f8c32b6eed3c531f98de5d
Shima Maleki <92548365+shima-maleki@users.noreply.github.com>
2025-07-24 23:28:07 +0100

    Add files via upload

---
+++ vite-github-pages.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/shima-maleki-portfolio/', // Replace with your repository name
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './client/src'),
+      '@assets': path.resolve(__dirname, './attached_assets')
+    }
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './client/index.html'
+    }
+  }
+})
